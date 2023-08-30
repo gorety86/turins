e0719edd1cb94aa84c1ed5ed8eb362be6b89d16b
Gorety <wagutugorety@gmail.com>
2023-08-30 15:51:18 +0300

    made changes in about us

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from 'react';
+import './AboutUs.css';
 
 const AboutUs = () => {
   return (
-    <div>About Us</div>
-    
-  )
-}
+    <div className="about-us">
+      <div className="about-us-content">
+        <h2>ABOUT TURINS</h2>
+        <p>
+          <p>TURINS BYLATECH TECHNOLOGIES is widely recognized as a dynamic general supply.</p>
+        <p>ICT training and consultancy, hardware maintenance, and software Development</p> 
+        <p>Company. We are experts in the provision of IT-Enabled solutions, services, and training around </p>
+        <p>the world.</p>
+        </p>
+        <p>
+          <p>It has been offering the global community the finest of software products and solutions that</p> 
+          <p>not only fulfill their needs today but offer them a competitive advantage tomorrow. <p/>
+          <p>We started with the aim of providing a world-class service at an affordable price without</p>
+          <p> compromising strength and reliability. We are now recognized as one of the leading general supply,</p>
+          <p>hardware maintenance, Web, SMS & Application solution providers in the industry.</p>
+        </p>
+        </p>
+        <p>
+          <p>We also understand that every successful company is a team of skilled and dedicated people.</p> 
+          <p>We have the best team of Hardworking, Honest, Talented, and Certified professionals with the </p>
+          <p>necessary skills and drive to provide our clients with superior services.</p>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
